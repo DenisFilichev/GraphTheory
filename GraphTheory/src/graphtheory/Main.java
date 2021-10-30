8f3ef3b8edaf08960c96fe8820600a5c54a96aa8
DenisFilichev <denis.filic@gmail.com>
2021-10-30 19:37:43 +0300

    Простой пример теории графов

---
+++ GraphTheory/src/graphtheory/Main.java
@@ -0,0 +1,16 @@
+package graphtheory;
+
+/**
+ *
+ * @author denis
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
